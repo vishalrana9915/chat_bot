6ef3d8d252d7355e7277bf0761cbfc22a0d8b0a7
Vinn Rana (Unifyed) <Vinn_Rana@unifyed.com>
2018-02-12 22:25:38 +0530

    register done

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
-'use strict';
-
-
-const express = require('express')
-const bodyParser = requrie('body-parser')
-const cors = require('cors');
-const app  = express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-
-app.set('port',7595)
+console.log("");
+console.log("//************************* Chat Bot **************************//");
+console.log("");
 
 
 
-app.listen(app.get('port'),()=>{
-})
+
+//Import Config
+const config = require('./lib/config');
+
+// Import logger
+var logger = require('./lib/logger').logger;
+
+// logger.requestLogger;
+
+config.dbConfig(config.cfg, (err) => {
+    if (err) {
+        logger.error(err, 'exiting the app.');
+        return;
+    }
+
+console.log("running")
+
+    // load external modules
+    const express = require("express");
+
+    // const mediaUpload = require("./lib/mediaupload/configure")();
+    // mediaUpload();
+
+    // init express app
+    const app = express();
+
+    // set server home directory
+    app.locals.rootDir = __dirname;
+
+    // config express
+    config.expressConfig(app, config.cfg.environment);
+
+    // attach the routes to the app
+    require("./lib/route")(app);
+    //require("./lib/post")(app);
+
+    // start server
+    app.listen(config.cfg.port, () => {
+        logger.info(`Express server listening on ${config.cfg.port}, in ${config.cfg.TAG} mode`);
+    });
+
+});
